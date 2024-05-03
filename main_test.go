b06c1af71cb4623bef7f4369fa91034b2e12679c
Tobias Fischer <tobias@tformatix.at>
2024-05-03 13:40:59 +0200

    Fix SonarCloud

--- main_test.go
+++ main_test.go
@@ -16,9 +16,9 @@ var app App
 
 func TestMain(m *testing.M) {
 	app.Initialize(
-		"postgres",
-		"postgres",
-		"postgres")
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	ensureTableExists()
 	code := m.Run()
